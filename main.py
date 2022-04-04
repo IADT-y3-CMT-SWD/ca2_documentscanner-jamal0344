5337f0370ef27064bf1933b75f5bb7b53f0e3160
jamal0344 <n00171193@student.iadt.ie>
2022-04-04 15:40:34 +0100

    Doc scanner

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+import cv2
+
+########################################################
+webCamFeed = False
+pathImage = "Images\\images004.jpg"
+cap = cv2.VideoCapture(0)
+cap.set(10,160)
+heightImg = 640
+widthImg = 480
+#####################################################
